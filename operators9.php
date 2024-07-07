6f610e020a591f24ec18a0c876147a140d974231
shekh farid <shekhfarid1974@gmail.com>
2024-07-07 23:32:11 +0600

    operators9

--- operators9.php
+++ operators9.php
@@ -2,16 +2,24 @@
 //  operators in php
 
 // 1. Arithmetic Operators
-// 2.Assignment Operators
+// 2. Assignment Operators
 // 3. Logical Operators
 
 $a = 45;
 $b = 8;
 
-echo $a + $b;
+// 1. Arithmetic Operators
+echo "For a + b, the resutl is". $a + $b . "<br>";
+echo "For a - b, the resutl is". $a - $b . "<br>";
+echo "For a * b, the resutl is". $a * $b . "<br>";
+echo "For a / b, the resutl is". $a / $b . "<br>";
+echo "For a % b, the resutl is". $a % $b . "<br>";
+echo "For a ** b, the resutl is". $a ** $b . "<br>";
 
 
-
+// 2. Assignment Operators
+$x = $a;
+echo "For x, the value is". $x . "<br>";
 
 
 
