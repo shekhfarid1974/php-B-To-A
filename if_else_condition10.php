fed6beff0cfc7dabfeb8fa45378d6da230f0b6d8
shekh farid <shekhfarid1974@gmail.com>
2024-07-15 15:02:49 +0600

    if else

--- if_else_condition10.php
+++ if_else_condition10.php
@@ -10,18 +10,27 @@
 //     echo "a is not grater then 80";
 // }
  
-$age = 15; //value 54, 18, 15
+// $age = 15; //value 54, 18, 15
 
-if ($age > 18){
-    echo "you can drink coffee with water and alcohol";
-}
-elseif($age > 16){
-    echo "you can drink coffee with water no alcohol";
+// if ($age > 18){
+//     echo "you can drink coffee with water and alcohol";
+// }
+// elseif($age > 16){
+//     echo "you can drink coffee with water no alcohol";
+// }
+// else{
+//     echo "you can drink only water";
+// }
+
+// Question:- write a program to allow a driver to drive only when his age is greter than or equal to 25 and less than or equal to 65
+
+$age = 26;
+
+if ($age >= 25 && $age <= 65){
+    echo "you can drive car or motorcycle";
 }
 else{
-    echo "you can drink only water";
+    echo "you can not drive you are old";
 }
-
-
 
 ?>
