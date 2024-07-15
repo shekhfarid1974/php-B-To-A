99d40e6356c257e658d19f3e14846d68d1bea840
shekh farid <shekhfarid1974@gmail.com>
2024-07-15 14:43:46 +0600

    elseif ladder

--- if_else_condition10.php
+++ if_else_condition10.php
@@ -1,15 +1,27 @@
 <?php
 
-$a = 90;
-$b = 9;
+// $a = 90;
+// $b = 9;
 
-if($a > 80){ //Example: 7 > 5 means "7 is greater than 5"
-    echo "a is grater then 80";
+// if($a > 80){ //Example: 7 > 5 means "7 is greater than 5"
+//     echo "a is grater then 80";
+// }
+// else{
+//     echo "a is not grater then 80";
+// }
+ 
+$age = 15; //value 54, 18, 15
+
+if ($age > 18){
+    echo "you can drink coffee with water and alcohol";
+}
+elseif($age > 16){
+    echo "you can drink coffee with water no alcohol";
 }
 else{
-    echo "a is not grater then 80";
+    echo "you can drink only water";
 }
- 
+
 
 
 ?>
