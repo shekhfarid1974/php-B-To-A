d4e91c50aa3fc2d4dc566eed89122faa8d6864c1
shekh farid <shekhfarid1974@gmail.com>
2024-07-15 14:31:52 +0600

    grater than

---
+++ if_else_condition10.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 90;
+$b = 9;
+
+if($a > 80){ //Example: 7 > 5 means "7 is greater than 5"
+    echo "a is grater then 80";
+}
+else{
+    echo "a is not grater then 80";
+}
+ 
+
+
+?>
